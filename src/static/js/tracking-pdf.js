1708f63413710e3ff0e55b8146638404416de5c6
Toshiaki Hishinuma <hishinuma.toshiaki@gmail.com>
2020-02-16 03:55:32 +0900

    update pub links

--- src/static/js/tracking-pdf.js
+++ src/static/js/tracking-pdf.js
@@ -10,24 +10,36 @@
 // 	});
 // });
 //
-if (typeof jQuery != 'undefined') {
-	console.info('jQuery detected');
-	jQuery(document).ready(function($) {
-		var filetypes = /\.(apk|ipa|zip|exe|pdf|doc*|xls*|ppt*|mp3)$/i;
-		var baseHref = '';
-		if (jQuery('base').attr('href') != undefined)
-			baseHref = jQuery('base').attr('href');
-		jQuery('a').each(function() {
-			var href = jQuery(this).attr('href');
-			if (href && (href.match(/^https?\:/i)) && (!href.match(document.domain))) {
-				jQuery(this).click(function() {
-					var extLink = href.replace(/^https?\:\/\//i, '');
-					ga('send', 'event', 'download', extLink);
-				});
-			}
-		});
+// if (typeof jQuery != 'undefined') {
+// 	console.info('jQuery detected');
+// 	jQuery(document).ready(function($) {
+// 		var filetypes = /\.(apk|ipa|zip|exe|pdf|doc*|xls*|ppt*|mp3)$/i;
+// 		var baseHref = '';
+// 		if (jQuery('base').attr('href') != undefined)
+// 			baseHref = jQuery('base').attr('href');
+// 		jQuery('a').each(function() {
+// 			var href = jQuery(this).attr('href');
+// 			if (href && (href.match(/^https?\:/i)) && (!href.match(document.domain))) {
+// 				jQuery(this).click(function() {
+// 					var extLink = href.replace(/^https?\:\/\//i, '');
+// 					ga('send', 'event', 'download', extLink);
+// 				});
+// 			}
+// 		});
+// 	});
+// }
+// else {
+// 	console.info('jQuery undetected');
+// }
+
+function() {
+	$("a").on("click", function() {
+		var url = this.href;
+		if (url && url.match(/\.pdf$/)) {
+			ga('send', {
+				'hitType': 'event', 'download', url
+			});
+		}
 	});
-}
-else {
-	console.info('jQuery undetected');
-}
+});
+
